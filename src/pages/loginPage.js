255f577a31b7bf64132f74ed16f64d5020aa21de
sasing <vencejumar.sasing.20@usjr.edu.ph>
2023-09-25 11:14:05 +0800

    implenented login page

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 import styles from "@/styles/Login.module.css";
 import Image from "next/image";
 import shiftLoginImage from "../assets/web login image2.jpg";
-import shiftLogoWithText from "../assets/capstone logo with text.png";
+import shiftLogoWithText from "../assets/capstone logo with text white.png";
 import shiftLoginExtraImg from "../assets/login extra1.png";
 
 const LoginPage = () => {
@@ -34,41 +34,30 @@ const LoginPage = () => {
             Weather-Informed Routes for Flood Susceptible Destinations
           </p>
         </div>
-        <form className={styles.loginForm}>
-          <div className={styles.loginFormFields}>
-            <table>
-              <tr className={styles.rowContainer}>
-                <label className={styles.fieldLabel} htmlFor="username">
-                  Username:
-                </label>
-              </tr>
-              <tr className={styles.rowContainer}>
-                <input
-                  className={styles.fieldInput}
-                  type="text"
-                  id="username"
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                />
-              </tr>
-              <tr className={styles.rowContainer}>
-                <label className={styles.fieldLabel} htmlFor="password">
-                  Password:
-                </label>
-              </tr>
-              <tr className={styles.rowContainer}>
-                <input
-                  className={styles.fieldInput}
-                  type="password"
-                  id="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </tr>
-              <tr className={styles.rowContainer}>
-                <label className={styles.fieldLabelBtn}>Forgot password?</label>
-              </tr>
-            </table>
+        <div className={styles.loginFormFields}>
+          <form className={styles.loginForm}>
+            <label className={styles.fieldLabel} htmlFor="username">
+              Username:
+            </label>
+            <input
+              className={styles.fieldInput}
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label className={styles.fieldLabel} htmlFor="password">
+              Password:
+            </label>
+            <input
+              className={styles.fieldInput}
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <label className={styles.fieldLabelBtn}>Forgot password?</label>
+
             <div className={styles.loginButtonContainer}>
               <button
                 className={styles.loginButton}
@@ -78,7 +67,7 @@ const LoginPage = () => {
                 Login
               </button>
             </div>
-          </div>
+          </form>
           <div className={styles.loginExtraImageContainer}>
             <Image
               className={styles.loginExtraImage}
@@ -86,7 +75,7 @@ const LoginPage = () => {
               alt="shift login image"
             />
           </div>
-        </form>
+        </div>
       </div>
       <div className={styles.loginImageContainer}>
         <Image
